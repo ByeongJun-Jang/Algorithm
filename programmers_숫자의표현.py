f27919f0e3e51c4304dd00c6d07271183fe45ba9
ByeongJun-Jang <85213981+ByeongJun-Jang@users.noreply.github.com>
2023-11-23 13:09:23 +0900

    Create programmers_숫자의표현.py

---
+++ programmers_숫자의표현.py
@@ -0,0 +1,27 @@
+data = int(input())
+answer = 0
+for i in range(1,data+1):
+  sum=0
+  for k in range(i,data+1):
+    sum += k
+    if sum == data:
+      answer += 1
+    elif sum > data:
+      break
+
+answer
+# 완전 탐색을 활용하여 풀어야 하는 문제
+
+def solution(data):
+  data = int(input())
+  answer=0
+  for i in range(1,data+1):
+    sum = 0
+    for k in range(i,data+1):
+      sum += k
+      if sum == data:
+        answer += 1
+      elif sum > data:
+        break
+
+  return data
